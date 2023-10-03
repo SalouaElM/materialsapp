d4325a5d4c6dcd812c55f8f79140dc2104b436f4
SalouaElM <91462849+elmsaloua@users.noreply.github.com>
2023-10-03 21:01:15 +0200

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+
+sap.ui.require([
+	"be/ap/sap/materialsapp/test/integration/AllJourneys"
+], function() {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/integration/pages/Common.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/test/Opa5"
+], function(Opa5) {
+	"use strict";
+
+	return Opa5.extend("be.ap.sap.materialsapp.test.integration.pages.Common", {
+
+	});
+
+});
